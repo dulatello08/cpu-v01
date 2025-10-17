74c8a095a900286dea32ff2ea79af0af4d426c1e
Dulat <dulatello08@gmail.com>
2025-10-17 15:01:35 -0700

    Initial commit

---
+++ top.sv
@@ -0,0 +1,15 @@
+/*
+* Copyright (c) 2025. All rights reserved.
+* Created by dulat, 10/17/25
+*/
+
+module top #(
+    parameter [7:0] WIDTH = 8'h0
+) (
+    input  wire [7:0] in,
+    output wire [7:0] out
+);
+
+    assign out = in;
+
+endmodule
